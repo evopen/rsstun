16065beb510de38f80b499ec28834927228d5408
evopen <520dhh@gmail.com>
2021-04-09 18:00:13 +0800

    update

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,20 @@
+use std::net::{IpAddr, SocketAddr};
 
+use net2::UdpSocketExt;
+use std::net::UdpSocket;
+
+#[repr(u16)]
+enum StunMessage {
+    BindingRequest = 0x0001,
+    BindingResponse = 0x0101,
+}
+
+fn stun_test(socket: &UdpSocket) {}
+
+fn get_nat_type(socket: &UdpSocket, stun_host: &str, stun_port: u16) {
+    let ret = stun_test(socket);
+}
+
+pub fn get_ip_info(source_ip: IpAddr, source_port: u16, stun_host: &str, stun_port: u16) {
+    let socket = UdpSocket::bind(SocketAddr::from((source_ip, source_port))).unwrap();
+}
